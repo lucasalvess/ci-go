ede6297092752ae682aad25eda98d76284217c7a
Lucas Alves <lucasalves@MacBook-Air-de-Lucas.local>
2024-06-08 12:19:33 -0300

    Fix test with wrong function name

--- math_test.go
+++ math_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	total := sumNumber(15, 15)
+	total := sum(15, 15)
 
 	if total != 30 {
 		t.Errorf("Result of sum is invalid: Result %d. Expected: %d", total, 30)
