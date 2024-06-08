224ee9309d1f74c73544988f53cdaa3ab37b8497
Lucas Alves <lucasalves@MacBook-Air-de-Lucas.local>
2024-06-08 12:18:14 -0300

    Change name of sum function in test for break CI flux

--- math_test.go
+++ math_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	total := sum(15, 15)
+	total := sumNumber(15, 15)
 
 	if total != 30 {
 		t.Errorf("Result of sum is invalid: Result %d. Expected: %d", total, 30)
